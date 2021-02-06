04b6263b791d0fa4722faee9bba00c4972909814
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-05 22:13:42 -0800

    starting with initial setup

---
+++ build/precache-manifest.1f027a282e69994654f36c9642b9c259.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca34d484fa554909b8c07aacb54679c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b3f323a3564fc6682a",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "64ea0afce42a254f6ad3",
+    "url": "/static/js/2.c1258a52.chunk.js"
+  },
+  {
+    "revision": "21b3f323a3564fc6682a",
+    "url": "/static/js/main.7560e3db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
